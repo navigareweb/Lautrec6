b3782a9333c1fc34ff64ceb800e3e6c776e9d582
navigareweb <web@navigaresrl.com>
2024-04-11 10:38:19 +0200

    first commit

--- app/page.js
+++ app/page.js
@@ -1,94 +1,48 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import styles from "./index.module.scss";
 
 export default function Home() {
   return (
     <main className={styles.main}>
+      <Image
+        src={"/400x600.jpeg"}
+        width={600}
+        height={600}
+        className={styles.img}
+        alt="imgLautrec"
+      ></Image>
       <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
+        <h3 className={styles.text}>PORTRAITS</h3>
+        <audio controls src="/PORTRAITS.mp3" className={styles.audio}>
+          Your browser does not support the
+          <code>audio</code> element.
+        </audio>
+        <p className={styles.p}>
+          Being the author of a unique body of works, Toulouse-Lautrec captures
+          in these works - as if they were photo snapshots - different aspects
+          of daily life in Parisian neighborhoods: a multicultural accumulation
+          marked by attitudes, gestures and shapes that convey the truest
+          essence of life. Street scenes, interiors, couples, groups or single
+          figures are nearly secretly captured and laid bare in their
+          spontaneity. One of Lautrec's favorite places, despite his notorious
+          rejection of landscape painting as an artistic discipline, was the
+          Bois de Boulogne, where he spent many hours sketching members of all
+          levels of society walking, horse riding, bicycling or taking public
+          transportation, and who are part of this sometimes-unclassifiable
+          work. The study of the different poses, gestures, and environment that
+          surrounded these subjects shows Lautrec's formal attention, his
+          aesthetic interests, and his taste for the atmosphere, which led him
+          to develop a peculiar and unique working technique which was marked by
+          rapid live-taking. In the sketches made in pastel, sanguine or pencil
+          presented in the exhibition, Toulouse-Lautrec's skill as a drawer is
+          more than clear. His concise, rapid and incisive stroke manages to
+          render in a few marks a whole human story or a very rich atmosphere,
+          even in the lack of details. Furthermore, his great skill as a drawer
+          would be of paramount importance in his graphic work: Lautrec almost
+          always drew directly on stone, and it was rare to see him use
+          autographic paper, which would have been impossible without the
+          drawing skill he had developed since he was a child.
         </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
       </div>
     </main>
   );
